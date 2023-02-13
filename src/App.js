3cef78f24984c78442c34c8d4fde470fa1ca146f
Sirirat Wisessiri <sirirat.wisessiri@avanade.com>
2023-02-13 19:27:38 +1100

    Add image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import image from './img-2.jpg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={image}/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
